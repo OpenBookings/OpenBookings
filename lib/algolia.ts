2db7b8b4bd647ae69bddcaf8921f5ecb02df14b8
Wouter van der Wal <81486986+wouter1203@users.noreply.github.com>
2025-12-29 16:24:23 +0100

    Integrate Algolia search with instant search in SearchBar
    Refactored SearchBar to use Algolia's react-instantsearch-hooks-web for instant search functionality. Added a new lib/algolia.ts for Algolia client configuration. Updated dependencies to include algoliasearch and react-instantsearch-hooks-web.

---
+++ lib/algolia.ts
@@ -0,0 +1,8 @@
+import { liteClient } from 'algoliasearch/lite';
+
+export const searchClient = liteClient(
+  'KVLTEZ2BF7',
+  '4c2dfdc2007ec035fdef0b008196a19e'
+);
+
+export const ALGOLIA_INDEX_NAME = 'Cities';
