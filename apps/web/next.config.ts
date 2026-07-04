5c9d1c47d0a7f8ef3c5287129170cc016f866c60
Wouter van der Wal <81486986+wouter1203@users.noreply.github.com>
2026-07-04 10:56:58 +0200

    Update next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -5,7 +5,7 @@ import { withSentryConfig } from "@sentry/nextjs";
 const ContentSecurityPolicy = `
   default-src 'self';
   script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn-cookieyes.com https://*.openbookings.co https://eu-assets.i.posthog.com https://internal-j.posthog.com;
-  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://*.openbookings.co;
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://*.openbookings.co https://eu-assets.i.posthog.com;
   img-src 'self' data: blob: https://images.openbookings.co https://cdn.openbookings.co https://cdn-cookieyes.com https://*.google.com https://*.googleusercontent.com https://*.openbookings.co;
   font-src 'self' https://fonts.gstatic.com;
   connect-src 'self' https://basemaps.cartocdn.com https://*.basemaps.cartocdn.com https://cdn-cookieyes.com https://*.cookieyes.com https://*.algolia.net https://*.i.posthog.com https://*.openbookings.co https://internal-j.posthog.com https://*.posthog.com https://*.maptiler.com;
