d7d2ad62c899f437fb52bb25818ef6d48b57db2e
Wouter van der Wal <81486986+wouter1203@users.noreply.github.com>
2026-07-04 15:15:33 +0200

    Update next.config.ts

--- apps/business/next.config.ts
+++ apps/business/next.config.ts
@@ -4,8 +4,8 @@ import { withSentryConfig } from "@sentry/nextjs";
 
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.openbookings.co https://eu-assets.i.posthog.com https://connect-js.stripe.com https://js.stripe.com;
-  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com sha256-0hAheEzaMe6uXIKV4EehS9pu1am1lj/KnnzrOYqckXk=;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.openbookings.co https://eu-assets.i.posthog.com https://*.posthog.com https://connect-js.stripe.com https://js.stripe.com;
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://eu-assets.i.posthog.com;
   img-src 'self' data: blob: https://cdn.openbookings.co https://*.google.com https://*.googleusercontent.com https://*.maptiler.com https://*.stripe.com https://api.dicebear.com https://storage.googleapis.com;
   font-src 'self' https://fonts.gstatic.com;
   connect-src 'self' https://*.i.posthog.com https://*.openbookings.co https://*.posthog.com https://api.maptiler.com https://basemaps.cartocdn.com https://connect-js.stripe.com https://storage.googleapis.com;
